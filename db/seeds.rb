0b6a75a502b9348760524db1006b0104b3d29e7d
github username <github email address>
2019-04-28 12:01:58 -0700

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+creators = Creator.create([ {creator_name: 'Shane Dawson'},
+  {creator_name: 'Gabe'},
+  {creator_name: 'Christine Barger'},
+  {creator_name: 'Pewty Pie'},
+  {creator_name: 'Logan Paul'},
+  {creator_name: 'Jake Paul'} ])
