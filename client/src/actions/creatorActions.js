abf66b2e37a26e004b4c7a719a5a063581a43afa
github username <github email address>
2019-04-28 15:34:56 -0700

    created an action to fetch creators from api

---
+++ client/src/actions/creatorActions.js
@@ -0,0 +1,10 @@
+export const fetchCreators = () => {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_CREATORS'});
+    return fetch('/api/creators')
+    .then(response => response.json())
+    .then(creators => {
+      return dispatch({ type: 'FETCH_CREATORS', creators: creators })
+    })
+  }
+}
