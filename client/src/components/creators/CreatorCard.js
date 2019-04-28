05d141264f127569fa1313f5e27ce2460f1f8b1c
github username <github email address>
2019-04-27 19:57:36 -0700

    add trending button

--- client/src/components/creators/CreatorCard.js
+++ client/src/components/creators/CreatorCard.js
@@ -4,13 +4,28 @@ import BackCard from './BackCard.js';
 
 export default class CreatorCard extends Component {
 
+  state = {
+    trending: 0
+  }
+
+  handleClick = () =>{
+    this.setState({
+      trending: this.state.trending + 1
+    })
+  }
+
   render() {
 
     return (
-      <div className="creator-card">
-        <FrontCard name={this.props.name} trending={this.props.trending}/>
-        <BackCard mostPopularVideo={this.props.mostPopularVideo} category={this.props.category}/>
+      <div>
+        <div className="creator-card">
+          <FrontCard name={this.props.name} trending={this.state.trending}/>
+          <BackCard mostPopularVideo={this.props.mostPopularVideo} category={this.props.category}/>
+        </div>
+            <button className="div-button" onClick={this.handleClick}>trend</button>
+
       </div>
+
     )
   }
 }
