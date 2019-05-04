7fb31e163663f0d175388b1e28cd196a4ac56294
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-04 14:42:28 -0700

    moved creators out of app to only have routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { fetchCreators } from './actions/creatorActions';
-import Creators from './containers/creators/Creators';
-import Routes from './routes/routes';
+
+import Routes from './routes/Routes';
 
 
 class App extends Component {
 
-  componentDidMount(){
-    this.props.fetchCreators()
-
-  }
-
   render() {
     return (
       <div className="App">
-        <Routes />
 
+       <Routes />
 
-        // <Creators creatorObjects={this.props.creators}/>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    creators: state.creators
-   }
- }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchCreators: () => dispatch(fetchCreators())
-//     }
-//   }
-export default connect(mapStateToProps, { fetchCreators })(App);
+export default App
