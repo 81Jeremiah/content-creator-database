929aea05e82249a91fdfe103b7e3bc8c902f50fb
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-05 15:16:14 -0700

    rendering only the attributes currenly available

--- client/src/components/creators/CreatorDetails.js
+++ client/src/components/creators/CreatorDetails.js
@@ -1,36 +1,18 @@
 import React from 'react';
 
+
 const CreatorDetails = props => {
-  const { name, image, platform, bio, video, catrgory, collabsWith } = props.creator
+  const { creator_name } = props.creator
 
   return (
-    <div className="tc ph4">
-    <h2 className="f3 f2-m f1-l fw2 black-90 mv3">
-      { name }
+    <div className="">
+    <h2 className="">
+      { creator_name }
     </h2>
 
       <p>
-        { image }
+
       </p>
-
-      <p>
-        { platform }
-      </p>
-
-      <div>
-        <label> </label>
-      </div>
-
-      <div>
-        <label></label>
-        <p>
-          { bio }
-        </p>
-      </div>
-
-      // <button  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib dark-green" onClick={props.handleDeleteRecipe}>
-      //   Delete creator
-      // </button>
 
     </div>
   )
