eae0ad42d327e909d2c312b8f1d552b13e553bed
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-04 17:09:48 -0700

    updated action to fetch single creator

--- client/src/actions/creatorActions.js
+++ client/src/actions/creatorActions.js
@@ -6,22 +6,18 @@ export const fetchCreators = () => {
     .then(creators => {
       return dispatch({ type: 'FETCH_CREATORS', creators: creators })
     })
-
   }
 }
+ export const fetchCreator = (creatorId) => {
+    return (dispatch) => {
+      dispatch({type: 'LOADING_CREATOR'});
+      return fetch(`/api/creators/${creatorId}`)
+      .then(response => response.json())
+      .then(creator => {
+        return dispatch({ type: 'FETCH_CREATOR', creator: creator })
+      })
 
-export const updateTrending = (creatorId) => {
-
-}
-
-export const fetchCreator = (creatorId) => {
-  return (dispatch) => {
-    dispatch({type: 'LOADING_CREATOR'});
-    return fetch(`/api/creators/${creatorId}`)
-    .then(response => response.json())
-    .then(creator => {
-      return dispatch({ type: 'FETCH_CREATOR', creators: creator })
-    })
-
+    }
   }
-}
+
+ 
