4cf0eda57a60b8c33bab99e8a2ca369a769e037a
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-05 15:17:44 -0700

    updated routes to route to show page

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import  CreatorForm  from '../components/creators/CreatorForm';
 import  TopCreators from '../components/creators/TopCreators';
-
+import CreatorPage from '../components/creators/CreatorPage'
 
 const Routes = props => {
 
@@ -24,10 +24,11 @@ const Routes = props => {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <div class='container'>
+            <div className='container'>
             <Switch>
                <Route exact path="/new" component={CreatorForm}/>
                <Route exact path="/home" component={TopCreators}/>
+             <Route exact path="/creators/:id" component={CreatorPage} />
             </Switch>
             </div>
         </div>
