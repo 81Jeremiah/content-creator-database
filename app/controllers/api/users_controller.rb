ead1a53bdc9449430aec11f0cb6d1d1b5f270529
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-07 09:29:15 -0700

    added actions in user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,44 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+  before_action :set_user, only: [:show, :update]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(@user.email, @user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  def show
+    render json: @user
+  end
+
+  def find
+    @user = User.find_by(email: params[:email])
+    if @user
+      render json: @user
+    else
+      @errors = @user.errors.full_messages
+      render json: @errors
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
+
 
 end
