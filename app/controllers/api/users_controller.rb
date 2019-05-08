640a6102897d40521dd21fb76646308787306023
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-08 09:35:32 -0700

    upadated user controller to repond in json

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,10 +8,10 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(@user.email, @user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
+    @user = User.create(user_params)
+    # auth_token = AuthenticateUser.new(@user.email, @user.password).call
+    # response = { message: Message.account_created, auth_token: auth_token }
+    render json:@user
   end
 
   def show
