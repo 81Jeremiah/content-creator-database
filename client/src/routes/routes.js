c14f48d5494a82bf4a489734446c475853956177
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-09 09:33:52 -0700

    added react routes for signup page

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import  CreatorForm  from '../components/creators/CreatorForm';
-import  TopCreators from '../components/creators/TopCreators';
-import CreatorPage from '../components/creators/CreatorPage'
-import Login from '../containers/Login'
+import CreatorForm  from '../components/creators/CreatorForm';
+import TopCreators from '../components/creators/TopCreators';
+import CreatorPage from '../components/creators/CreatorPage';
+import Login from '../containers/Login';
+import SignUp from '../containers/SignUp';
+
 const Routes = props => {
 
     const something = (
@@ -27,7 +29,8 @@ const Routes = props => {
             <div className='container'>
             <Switch>
                <Route exact path="/new" component={CreatorForm}/>
-               <Route exact path="/login" componet={Login}/>
+               <Route exact path="/login" component={Login}/>
+               <Route exact path="/Signup" component={SignUp}/>
                <Route exact path="/" component={TopCreators}/>
                <Route exact path="/creators/:id" component={CreatorPage} />
             </Switch>
