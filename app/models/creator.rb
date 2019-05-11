a64a2ae680abdba4a6ae0265cc8608c07c0d0d07
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-11 16:07:46 -0700

    updated model to use active storage methods

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,2 +1,8 @@
 class Creator < ApplicationRecord
-end
+  has_one_attached :image
+#   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+#   validates_attachment :image, presence: true
+#   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+#
+#   do_not_validate_attachment_file_type :image
+ end
