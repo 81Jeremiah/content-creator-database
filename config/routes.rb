1c91d50bdad142e583d9b3fb0f73ef569c52788c
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-11 16:08:44 -0700

    switch to rails 5.2 to use active storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+ namespace :api do
+   resources :creators, :users
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :api do
-    resources :creators, :users
-
-  end
-
-
-
 end
