19fe3aec59085a1589ec4503ac85c7fe53142491
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-12 10:15:49 -0700

    added dropzone compnent to form to uplaod images

--- client/src/components/creators/CreatorForm.js
+++ client/src/components/creators/CreatorForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Button,Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
-
+import Dropzone from 'react-dropzone'
+import ImageUploader from "./ImageUploader"
 import { connect } from 'react-redux'
 // import { withRouter, Redirect } from 'react-router-dom'
 import { createCreator } from '../../actions/creatorActions'
@@ -13,7 +14,8 @@ class CreatorForm extends Component {
 
     this.state = {
       creator_name: "",
-      image: "",
+      image: null,
+      preview: null,
       platform: "",
       bio: "",
       videos: [],
@@ -21,6 +23,7 @@ class CreatorForm extends Component {
       collabsWith: []
 
     }
+
   }
 
   handleChange = event => {
@@ -30,14 +33,27 @@ class CreatorForm extends Component {
      });
    }
 
+   onDrop = (acceptedFiles) => {
+     console.log(acceptedFiles[0]);
+
+     this.setState({
+        image: acceptedFiles[0]
+     });
+   }
+
    handleSubmit = event => {
      event.preventDefault()
-     const creator = this.state
+
+     const creator= new FormData();
+     creator.append('[creator]creator_name', this.state.creator_name)
+     creator.append('[creator]image', this.state.image)
+     
 
      this.props.createCreator(creator)
      this.setState({
        creator_name: "",
-       image: "",
+       image: null,
+       preview: null,
        platform: "",
        bio: "",
        videos: [],
@@ -51,13 +67,23 @@ class CreatorForm extends Component {
     <Form onSubmit={this.handleSubmit}>
       <Form.Group controlId="exampleForm.ControlInput1">
         <Form.Label>Creator Name:</Form.Label>
-        <Form.Control type="text" name="creator_name" value={this.state.creator_name} onChange={this.handleChange}/>
+      <Form.Control type="text" name="creator_name" value={this.state.creator_name} onChange={this.handleChange}/>
 
       <Form.Label>Image:</Form.Label>
-        <Form.Control type="file" name="image" value={this.state.image} onChange={this.handleChange}/>
+        <div className="text-center mt-5">
+          <Dropzone onDrop={this.onDrop} accept="image/png, image/gif,image/jpg,image/jpeg" >
+
+            {({getRootProps, getInputProps}) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                Click me to upload a file!
+              </div>
+            )}
+          </Dropzone>
+        </div>
 
       <Form.Label>Platform:</Form.Label>
-        <Form.Control type="text" name="platform" value={this.state.platform} onChange={this.handleChange}/>
+    <Form.Control type="text" name="platform" value={this.state.platform} onChange={this.handleChange}/>
 
       </Form.Group>
 
