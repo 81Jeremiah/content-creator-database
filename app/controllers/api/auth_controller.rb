56504d021e85b816190b47339bcdb65662da633f
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-15 09:18:45 -0700

    implementing json web tokens to keep sessions

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,26 @@
+class AuthController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    skip_before_action :authorized, only: [:create]
+
+
+
+    def create
+        #find user by email if present
+        @user=User.find_by(email: params[:email])
+        #check-em in
+        if @user
+
+            #create a payload for the JWT
+            payload={user_id: @user.id}
+
+            token=encode_token(payload)
+
+            #render specific json
+            render json: { user: @user, jwt: token }
+        else
+            render json: {'message':'wrong username or password'}
+        end
+    end
+
+
+end
