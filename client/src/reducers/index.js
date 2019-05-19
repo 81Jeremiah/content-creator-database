3dc1e7eda76489e5627af884ac0b9cb9cd05fcab
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-18 23:01:12 -0700

    added auth reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import creatorReducer from './creatorReducer'
 import userReducer from './userReducer';
+import authReducer from './authReducer'
 
 const rootReducer = combineReducers({
   creators: creatorReducer,
-  users: userReducer
+  users: userReducer,
+  auth: authReducer
 })
 
 export default rootReducer;
