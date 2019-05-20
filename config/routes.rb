4d2fdab2f67e39b2ba04b741674e6177e48963f5
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-19 17:55:04 -0700

    created a search route for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  namespace :api do
+   get  'creators/search' => 'creators#search'
    resources :creators, :users
    post 'user_token' => 'user_token#create'
    post 'find_user' => 'users#find'
+
  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
