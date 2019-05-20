0666fac24d750cbb262ce6b618e31b4337776668
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-19 17:50:57 -0700

    added creator list component to display search results

---
+++ client/src/components/creators/CreatorList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CreatorList = props  => {
+
+
+  const listCreators = props.creators.map( creator => {
+    return(
+    <li key={creator.id}>
+    <img className="thumbnail" src={creator.image.url} alt="creator"/>
+    <Link to={`creators/${creator.id}`}>{creator.creator_name}</Link>
+    </li>)
+    })
+  return(
+    <div>
+       <ul>
+         { listCreators }
+       </ul>
+    </div>
+  )
+}
+
+CreatorList.defaultProps = {
+  creators: []
+}
+
+
+export default CreatorList
