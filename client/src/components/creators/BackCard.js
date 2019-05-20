71136318b96e6ed57da8fe0830b74cad3dbe4567
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-19 20:33:29 -0700

    adding bio, category and platfrom to creator card

--- client/src/components/creators/BackCard.js
+++ client/src/components/creators/BackCard.js
@@ -6,14 +6,28 @@ export default class BackCard extends Component {
 
 
   render() {
+    let bio = this.props.bio || ""
+    console.log(bio)
+    let bioSm = bio.substring(0,120)+ "..."
+    // if (this.props.bio !== "" ) {
+    //    debugger
+    //    bioSm = this.props.bio.sub(0,120) + "..."
+    // }else {
+    //    bioSm = this.props.bio
+    // }
+    // const bioSm = bio.str.sub(0,120) + "..."
     return (
       <div className="card-back">
-        <h3 className="most-pop-vid">{this.props.mostPopularVideo}</h3>
-        <span />
+        <h1> </h1>
 
-        <span />
-        <h5 className="category">{this.props.category}</h5>
+      <p className="bio">Bio: {bioSm}</p>
+    <h5 className="platform">Main Platform: {this.props.platform}</h5>
+  <h5 className="category">Category: {this.props.category}</h5>
       </div>
     )
   }
+}
+
+BackCard.defaultProps = {
+  bio: ""
 }
