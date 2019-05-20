bdb4bbf49216873d7084e69283dfdeac19771a6f
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-19 17:53:21 -0700

    added search to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 import creatorReducer from './creatorReducer'
 import userReducer from './userReducer';
 import authReducer from './authReducer'
-
+import searchReducer from './searchReducer';
 const rootReducer = combineReducers({
   creators: creatorReducer,
   users: userReducer,
-  auth: authReducer
+  auth: authReducer,
+  search: searchReducer
 })
 
 export default rootReducer;
