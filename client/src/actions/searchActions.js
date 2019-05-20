c6da594f204e5c55f4079fe65cd51d6183f6e757
81Jeremiah <jeremiahlutes@gmail.com>
2019-05-19 17:50:19 -0700

    added search action for basic creator search

---
+++ client/src/actions/searchActions.js
@@ -0,0 +1,10 @@
+export const search = (query) => {
+  return (dispatch) => {
+    dispatch({type: 'SEARCHING'});
+    return fetch(`/api/creators/search?creator_name=${query}`)
+    .then(response => response.json())
+    .then(creators => {
+      return dispatch({ type: 'SEARCH_RESULTS', creators: creators })
+    })
+  }
+}
