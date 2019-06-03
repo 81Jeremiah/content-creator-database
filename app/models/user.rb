38faeea2d2c371f6edfaf3d1b6ed1e354e9a7a98
81Jeremiah <jeremiahlutes@gmail.com>
2019-06-02 19:46:41 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
 has_secure_password
 
+validates :username, presence: true, uniqueness: true
+validates :email, presence: true, uniqueness: true
+validates_presence_of :password_digest
+
 end
